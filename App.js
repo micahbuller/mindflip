df2ccd52d97cfe103aa56805ec1b4859aa4a059b
Micah Buller <bullermicah@gmail.com>
2022-01-04 21:28:05 -0500

    Got styling to work with tailind

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'tailwind-rn';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tw('flex-1 bg-blue-200 justify-center items-center')}>
+      <Text style={tw('text-black')}>Tailwind is working!</Text>
       <StatusBar style="auto" />
     </View>
   );
