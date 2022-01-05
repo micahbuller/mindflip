b11f9a4c103be367e8ca2100e3c1816b9095e821
Micah Buller <bullermicah@gmail.com>
2022-01-04 22:00:49 -0500

    don t know

--- App.js
+++ App.js
@@ -17,17 +17,17 @@ const [color, setColor] = useState('bg-blue-200')
     <View style={tw(`flex-1 ${color} justify-center items-center`)}>
       <Text style={tw('text-black')}>Tailwind is working!</Text>
       <View>
-      <TouchableOpacity 
-        styl={tw('bg-black py-3 m-3')} 
-        onPress={() => {
-          if(color == "bg-red-200"){
-            setColor("bg-blue-200")
-          }else{
-            setColor("bg-red-200")
-          }
-        }}>
-        <Text>Press me.</Text>
-      </TouchableOpacity>
+        <TouchableOpacity 
+          styl={tw('bg-black py-3 m-3')} 
+          onPress={() => {
+            if(color == "bg-red-200"){
+              setColor("bg-blue-200")
+            }else{
+              setColor("bg-red-200")
+            }
+          }}>
+          <Text>Press me.</Text>
+        </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
     </View>
