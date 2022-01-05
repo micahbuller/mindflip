dc83efe40724fd21c719c0bafdbacb1f9a90402b
Micah Buller <bullermicah@gmail.com>
2022-01-04 21:58:00 -0500

    Added a button, but styling for the button doesn't work

--- App.js
+++ App.js
@@ -1,22 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+  Image,
+} from "react-native";
+import React, { useState } from 'react';
 import tw from 'tailwind-rn';
 
 export default function App() {
+const [color, setColor] = useState('bg-blue-200')
+
   return (
-    <View style={tw('flex-1 bg-blue-200 justify-center items-center')}>
+    <View style={tw(`flex-1 ${color} justify-center items-center`)}>
       <Text style={tw('text-black')}>Tailwind is working!</Text>
+      <View>
+      <TouchableOpacity 
+        styl={tw('bg-black py-3 m-3')} 
+        onPress={() => {
+          if(color == "bg-red-200"){
+            setColor("bg-blue-200")
+          }else{
+            setColor("bg-red-200")
+          }
+        }}>
+        <Text>Press me.</Text>
+      </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
