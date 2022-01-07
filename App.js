261aa02539ef6c0ff4c9b1db8f73009a138e8d6b
Micah Buller <bullermicah@gmail.com>
2022-01-06 23:04:44 -0500

    Added flatlist with mock data

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { UserIcon } from 'react-native-heroicons/outline'
+import { UserIcon, MenuIcon, CheckIcon, InformationCircleIcon } from 'react-native-heroicons/outline'
 import {
   View,
   Text,
@@ -12,48 +12,103 @@ import {
 import React, { useState } from 'react';
 import tw from 'tailwind-rn';
 
+const items = [
+  {
+    id:0,
+    truth:"I am not enough for my girlfriend.",
+    lie:"She loves me and it is okay to ask her to tell me."
+  },
+  {
+    id:1,
+    truth:"There is nothing good for me.",
+    lie:"There are lots of things in this world that are up for grabs and I can get them."
+  },
+  {
+    id:2,
+    truth:"We are going down hill.",
+    lie:"We are going uphill, and uphill is hard, but the view is worth it."
+  },
+  {
+    id:3,
+    truth:"We are going down hill.",
+    lie:"We are going uphill, and uphill is hard, but the view is worth it."
+  },
+  {
+    id:4,
+    truth:"We are going down hill.",
+    lie:"We are going uphill, and uphill is hard, but the view is worth it."
+  }
+]
+  
 export default function App() {
-const [color, setColor] = useState('bg-gray-100')
+  const [color, setColor] = useState('bg-gray-100')
+
+  const renderItem = ({ item }) => {
+
+    return(
+      <View>
+        <View style={tw('flex-1')}>
+        <View style={tw('items-center justify-center')}>
+          <View style={tw('flex-row justify-between items-center bg-gray-100 w-96 px-2 py-5 rounded-t-xl text-lg')}>
+            <Text style={tw('text-lg')}>{item.truth}</Text>
+          </View>
+          <View style={tw('relative flex-row justify-between items-center bg-purple-200 w-96 px-2 py-5 rounded-b-xl text-lg')}>
+            <Text style={tw('text-lg')}>{item.lie}</Text>
+            <TouchableOpacity style={tw('absolute right-2 bottom-2')}>
+              <InformationCircleIcon style={tw('w-24 h-24 text-purple-400')} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      </View>
+    );
+  }
 
   return (
   
-    <SafeAreaView style={tw(`flex-1 ${color} justify-between items-center`)}>
+    <SafeAreaView style={tw(`flex-1 bg-purple-100`)}>
 
       <View style={tw('flex-row items-center justify-between px-5')}>
-        <UserIcon style={tw('text-black')}/>
-        <UserIcon style={tw('text-black')}/>
-      </View>
-      
-
-      <View style={tw('items-center justify-center')}>
-        <TextInput
-          style={tw('bg-purple-200 w-80 px-2 py-5 rounded-t-xl text-lg')}
-          placeholder='False thought...'
-          multiline
-          numberOfLines={3}
-        />
-        <TextInput
-          style={tw('bg-purple-100 w-80 px-2 py-5 rounded-b-xl text-lg')}
-          placeholder='Tell yourself the truth...'
-          multiline
-          numberOfLines={3}
-        />
-      </View>
-        
-      
-      <View>
-        <TouchableOpacity 
-          style={tw('bg-black py-3 m-3 rounded-md')} 
-          onPress={() => {
-            if(color == "bg-gray-100"){
-              setColor("bg-purple-300")
-            }else{
-              setColor("bg-gray-100")
-            }
-          }}>
-          <Text style={tw('text-white px-5')}>Press me.</Text>
+        <TouchableOpacity onPress={() => {console.log("Pressed")}}>
+          <MenuIcon style={tw('text-black')}/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {console.log("Pressed")}}>
+          <UserIcon style={tw('text-black')}/>
         </TouchableOpacity>
       </View>
+      
+      <View style={tw('my-10')}>
+        <View style={tw('items-center justify-center')}>
+          <View style={tw('flex-row justify-between items-center bg-gray-100 w-96 px-2 py-5 rounded-t-xl text-lg')}>
+            <TextInput
+              style={tw('text-lg')}
+              placeholder='What have you been telling yourself?'
+              multiline
+              numberOfLines={3}
+            />
+          </View>
+          <View style={tw('flex-row justify-between items-center bg-purple-200 w-96 px-2 py-5 rounded-b-xl text-lg')}>
+            <TextInput
+              style={tw('text-lg')}
+              placeholder='Tell yourself the truth...'
+              multiline
+              numberOfLines={3}
+            />
+            <TouchableOpacity>
+              <CheckIcon style={tw('w-24 h-24 text-purple-400')} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <FlatList
+          data={items}
+          keyExtractor={item => item.id}
+          renderItem={renderItem}
+        />
+      </View>
+      
       <StatusBar style="auto" />
     </SafeAreaView>
   );
