deb9a1851622a3322ffb03d62c349d3e6bc52a79
Micah Buller <bullermicah@gmail.com>
2022-01-25 07:18:03 -0500

    Added keyboard avoiding view to the log in screen

--- src/screens/LogIn.js
+++ src/screens/LogIn.js
@@ -4,6 +4,7 @@ import {
   SafeAreaView,
   TouchableOpacity,
   TextInput,
+  KeyboardAvoidingView,
 } from "react-native";
 import React from "react";
 import tw from "tailwind-rn";
@@ -16,52 +17,66 @@ const LogIn = ({ navigation }) => {
       resizeMode="cover"
       style={{ flex: 1, justifyContent: "center" }}
     >
-      <SafeAreaView style={tw(`flex-1 `)}>
-        <View style={tw('justify-center items-center')}>
-          <Text style={tw("text-3xl w-28")}>Wlcm to mindflip</Text>
-        </View>
-        <View style={tw("flex-1 justify-center items-center mx-5")}>
-          <View
-            style={tw(
-              "bg-gray-50 flex-row w-full opacity-75 px-2 py-5 rounded-xl text-lg mb-5"
-            )}
-          >
-            <TextInput
-              style={tw("mx-5 text-lg font-bold")}
-              placeholder="Email"
-              multiline
-              keyboardType="email-address"
-            />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={tw("flex-1")}
+      >
+        <SafeAreaView style={tw(`flex-1 `)}>
+          <View style={tw("flex-row items-center justify-end px-5")}>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("Home");
+              }}
+            >
+              <Text style={tw("text-2xl text-black")}>or sign up</Text>
+            </TouchableOpacity>
           </View>
-          <View
-            style={tw(
-              "bg-gray-50 flex-row w-full opacity-75 px-2 py-5 rounded-xl text-lg mb-5"
-            )}
-          >
-            <TextInput
-              style={tw("mx-5 text-lg font-bold")}
-              placeholder="Password"
-              secureTextEntry={true}
-              multiline
-              keyboardType="email-address"
-            />
+          <View style={tw("justify-center flex-1 items-center")}>
+            <Text style={tw("text-3xl w-28")}>Wlcm to mindflip</Text>
           </View>
-          <TouchableOpacity
-            style={tw("w-full")}
-            onPress={() => {
-              navigation.navigate("Home");
-            }}
-          >
+          <View style={tw("flex-1 justify-center items-center mx-5")}>
             <View
               style={tw(
-                "bg-gray-900 flex-row w-full items-center justify-center opacity-75 px-2 py-5 rounded-full text-lg"
+                "bg-gray-50 flex-row w-full opacity-75 px-2 py-5 rounded-xl text-lg mb-5"
               )}
             >
-              <Text style={tw("text-2xl text-white")}>Sign In</Text>
+              <TextInput
+                style={tw("mx-5 text-lg font-bold")}
+                placeholder="Email"
+                multiline
+                keyboardType="email-address"
+              />
             </View>
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
+            <View
+              style={tw(
+                "bg-gray-50 flex-row w-full opacity-75 px-2 py-5 rounded-xl text-lg mb-5"
+              )}
+            >
+              <TextInput
+                style={tw("mx-5 text-lg font-bold")}
+                placeholder="Password"
+                secureTextEntry={true}
+                multiline
+                keyboardType="email-address"
+              />
+            </View>
+            <TouchableOpacity
+              style={tw("w-full")}
+              onPress={() => {
+                navigation.navigate("Home");
+              }}
+            >
+              <View
+                style={tw(
+                  "bg-gray-900 flex-row w-full items-center justify-center opacity-75 px-2 py-5 rounded-full text-lg"
+                )}
+              >
+                <Text style={tw("text-2xl text-white")}>Sign In</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
+      </KeyboardAvoidingView>
     </ImageBackground>
   );
 };
