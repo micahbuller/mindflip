ac394695f1b74dcec0dd2513ab6764c3df531e42
Micah Buller <micahbuller@KWAdmins-MacBook-Pro.local>
2022-04-03 15:55:42 -0400

    Added example cards

--- src/screens/CardAdd.js
+++ src/screens/CardAdd.js
@@ -22,20 +22,55 @@ import { addCard } from "../services/cardHelper";
 const DATA = [
   {
     id: 1,
-    lie: "Some friends are going out to dinner this weekend and I wasn't invited. My friends don't like me. They think I am boring. I will end up having no friends.",
+    lie: "When you fail, you may tell yourself that you’re a complete failure.",
     truth:
-      "My friends have told me several times that they think I am fun and that I make them laugh. Other friends have not been invited to other activities. I do get invited to most things.",
+      "This is All-Or-Nothing thinking. You are not always a failure.",
   },
   {
     id: 2,
-    lie: "I made a suggestion at the weekly meeting and most people thought it wasn't a good idea. I have no good ideas. People think I am stupid. I am terrible at my work.",
+    lie: "When you’re rejected by someone you care about, you may tell yourself that you’re an unlovable loser.",
     truth:
-      "People at work think that I am capable and often have good ideas. I do my work well, but this wasn't one of my best ideas.",
+      "This is an Overgeneralization. You were rejected by only one person, there is a whole world out there.",
   },
   {
     id: 3,
-    lie: "What is CBT 0.1",
-    truth: "What is CBT 0.1",
+    lie: "You are loved by lots of people. When one person doesn't like you, you obsess over it for hours. It can often ruid the rest of your day.",
+    truth: "This is a Mental Filter. You are loved by lots of people, don't focus on the one out of a hundred.",
+  },
+  {
+    id: 4,
+    lie: "You were complimented by someone the other day. You told your self that they told you that only to make you feel good.",
+    truth: "This is a Mental Filter. You are loved by lots of people, don't focus on the one out of a hundred.",
+  },
+  {
+    id: 5,
+    lie: "You’re feeling shy at a party, you may tell yourself that other people don’t have to struggle with shyness.",
+    truth: "You are jumping to conclusions. You will have no idea what others are thinking and going through without asking.",
+  },
+  {
+    id: 6,
+    lie: "When you are procrastinating, you may also tell yourself that your efforts today wouldn’t amount to anything anyway, so you might as well put it off.",
+    truth: "This is Minimizing. Every little bit of effort counts towards your end goal. You never know how it will turn out until you start.",
+  },
+  {
+    id: 7,
+    lie: "You meet someone attractive and there is a romantic intoxication that leads you to believe that person is the person of your dreams.",
+    truth: "This is Emotional Reasoning. You are feeling this way in the moment. This person may be a nasty individual and you don't know who they really are.",
+  },
+  {
+    id: 8,
+    lie: "You may tell yourself that your values are the best values and that other people should think and feel the same way.",
+    truth: "This is a Should-Statement. You make yourself (or others) miserable with “should's,” “must's” or ”ought to’s.",
+  },
+  {
+    id: 9,
+    lie: "You may label yourself or someone you’re not getting along with as “a loser” or “a jerk.”",
+    truth: "This is Labeling, and is as extreme form of overgeneralization. You or that other person are not entirely defined by that label.",
+  },
+  {
+    id: 10,
+    lie: "You may beat up on yourself constantly, blaming yourself for every error and shortcoming.",
+    truth: "This is Blaming. Instead of blaming yourself and stopping there, use your energy to find creative solutions to your problems.",
   },
 ];
 
@@ -158,7 +193,7 @@ const CardAdd = () => {
             </Text>
           </KeyboardAvoidingView>
           {DATA.map((item) => (
-            <RenderItem item={item} />
+            <RenderItem key={item.id} item={item} />
           ))}
         </ScrollView>
       </SafeAreaView>
