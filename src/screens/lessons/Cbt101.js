3712f78eec5424f33d3aaeff18c7ada44f18f289
Micah Buller <micahbuller@KWAdmins-MacBook-Pro.local>
2022-04-07 19:53:47 -0400

    Added some text for the first lesson

--- src/screens/lessons/Cbt101.js
+++ src/screens/lessons/Cbt101.js
@@ -11,7 +11,10 @@ import {
 } from "react-native";
 import tw from "tailwind-rn";
 import { ImageBackground } from "react-native";
-import { ArrowCircleLeftIcon } from "react-native-heroicons/solid";
+import {
+  ArrowCircleLeftIcon,
+  ArrowSmDownIcon,
+} from "react-native-heroicons/solid";
 
 const Cbt101 = ({ navigation }) => {
   return (
@@ -39,12 +42,132 @@ const Cbt101 = ({ navigation }) => {
               <ArrowCircleLeftIcon style={[tw("text-black")]} />
             </TouchableOpacity>
           </View>
-          <View style={tw("h-64 w-full bg-white rounded-md overflow-hidden")}></View>
-          <Text style={[tw("pt-5 text-xl"), { fontFamily: "Mon-Cheri" }]}>
-            SO WHAT IS CBT?
+          <View
+            style={tw("h-64 w-full bg-white rounded-md overflow-hidden")}
+          ></View>
+          <Text
+            style={[
+              tw("py-5 text-xl uppercase max-w-xs"),
+              { fontFamily: "Mon-Cheri" },
+            ]}
+          >
+            A brief introduction to cognitive behavioral therapy.
           </Text>
-          <Text>
-            It is something so cool you will flip out!
+          <Text
+            style={[
+              tw("py-5 text-sm text-right uppercase pl-12 text-gray-800"),
+              { fontFamily: "Nanum-Gothic" },
+            ]}
+          >
+            Whether you are dealing with problems of avoidance, addiction,
+            self-limiting thinking, generalized anxiety, or a whole array of
+            cognitive issues, cognitive behavioral therapy can help.
+          </Text>
+          <Text
+            style={[
+              tw("py-5 text-sm text-left uppercase pr-12 text-gray-800"),
+              { fontFamily: "Nanum-Gothic" },
+            ]}
+          >
+            Cognitive behavioral therapy (CBT for short) is a long term solution
+            for many problems. It gives you the tool that you need to be able to
+            effectively handle the situations that life may throw your way.
+          </Text>
+          <Text
+            style={[
+              tw("py-5 text-lg text-center uppercase text-gray-800"),
+              { fontFamily: "Nanum-Gothic" },
+            ]}
+          >
+            The general struture for cbt is:
+          </Text>
+          <View style={tw("h-12 w-full bg-white rounded-full overflow-hidden")}>
+            <Text
+              style={[
+                tw(
+                  "pt-2 items-center text-lg text-center uppercase text-gray-800"
+                ),
+                { fontFamily: "Nanum-Gothic" },
+              ]}
+            >
+              Situation
+            </Text>
+          </View>
+          <View style={tw("flex flex-col items-center py-2")}>
+            <ArrowSmDownIcon style={[tw("text-gray-800")]} />
+          </View>
+          <View style={tw("h-12 w-full bg-white rounded-full overflow-hidden")}>
+            <Text
+              style={[
+                tw(
+                  "pt-2 items-center text-lg text-center uppercase text-gray-800"
+                ),
+                { fontFamily: "Nanum-Gothic" },
+              ]}
+            >
+              Automatic Thought
+            </Text>
+          </View>
+          <View style={tw("flex flex-col items-center py-2")}>
+            <ArrowSmDownIcon style={[tw("text-gray-800")]} />
+          </View>
+          <View style={tw("h-12 w-full bg-white rounded-full overflow-hidden")}>
+            <Text
+              style={[
+                tw(
+                  "pt-2 items-center text-lg text-center uppercase text-gray-800"
+                ),
+                { fontFamily: "Nanum-Gothic" },
+              ]}
+            >
+              Reaction
+            </Text>
+          </View>
+          <Text
+            style={[
+              tw("pt-12 text-sm text-right uppercase pl-12 text-gray-800"),
+              { fontFamily: "Nanum-Gothic" },
+            ]}
+          >
+            Example:
+          </Text>
+          <Text
+            style={[
+              tw("py-5 text-sm text-right uppercase pl-12 text-gray-800"),
+              { fontFamily: "Nanum-Gothic" },
+            ]}
+          >
+            (Situation) You see someone you know while walking down the street
+            and they don’t say hi back to you.
+          </Text>
+          <Text
+            style={[
+              tw("py-5 text-sm text-right uppercase pl-12 text-gray-800"),
+              { fontFamily: "Nanum-Gothic" },
+            ]}
+          >
+            (Automatic Thoughts) Your instinctive way of thinking takes over and
+            you may tell yourself “They must not like me because they ignored
+            me”.
+          </Text>
+          <Text
+            style={[
+              tw("py-5 text-sm text-right uppercase pl-12 text-gray-800"),
+              { fontFamily: "Nanum-Gothic" },
+            ]}
+          >
+            (Reaction) You may end up avoiding that person in the future and
+            losing contact with them totally.
+          </Text>
+          <Text
+            style={[
+              tw("py-5 text-sm text-left uppercase pr-12 text-gray-800"),
+              { fontFamily: "Nanum-Gothic" },
+            ]}
+          >
+            If we have more grounded and healthy automatic thoughts, we probably
+            would have said “They probably didn’t even see me or hear me” and
+            thought nothing more of it.
           </Text>
         </ScrollView>
       </SafeAreaView>
